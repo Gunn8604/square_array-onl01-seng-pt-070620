1e5c517cc751a2edb0589cbdf43789da6a25fa6f
Billy Gunn, Jr <bgunnjr@gmail.com>
2020-07-14 23:04:07 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -2,9 +2,9 @@ def square_array(array)
   numbers = [1,2,3]
   new_numbers = [9,10,16,25]
   square_array(numbers)
-    return numbers.collect(&**)
+    return numbers.collect(**)
   square_array(new_numbers)
-    return new_numbers.collect(&**)
+    return new_numbers.collect(**)
   end
 end
 
